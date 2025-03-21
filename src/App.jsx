201126d6e3a027d0eae69615a08b2675a892722e
Qiydaar23 <Qiydaarwilliams23@gmail.comgit>
2025-03-21 16:39:12 -0400

    backend and front end connected data mapped, mysql working header and footer added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+      <ListEmployeeComponent />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <headr>
+        <nav className='navbar navbar-dark bg-dark'>
+            <a className='navbar-brand' href=''>Employee Management System</a>
+        </nav>
+      </headr>
+    </div>
+  )
+}
+
+export default Header
